dbd01a55a11e0cee2c2bf174d218f0d2517a6808
quoctrungvu1610 <quoctrungvu1610@gmail.com>
2025-12-08 23:07:11 +0700

    Update URP

--- Assets/Assets/Scripts/Bullet.cs
+++ Assets/Assets/Scripts/Bullet.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private GameObject bulletImpactFX;
     private Rigidbody rb => GetComponent<Rigidbody>();
 
     private void OnCollisionEnter(Collision collision)
     {
-        //rb.constraints = RigidbodyConstraints.FreezeAll;
+        CreateImpactFX(collision);
         Destroy(gameObject);
+    }
+
+    private void CreateImpactFX(Collision collision)
+    {
+        if (collision.contacts.Length > 0) 
+        {
+            ContactPoint contact = collision.contacts[0];
+            Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+            Vector3 pos = contact.point;
+            GameObject impactFX = Instantiate(bulletImpactFX, pos, rot);
+            Destroy(impactFX, 1f);
+        }
     }
 }
